a696bdbc886017ebeae487fea3e835cfde1e1c35
imUmer <imumerniaz@gmail.com>
2020-12-07 11:23:31 +0500

    Changes in urls.py and views.py

--- mywebsite/urls.py
+++ mywebsite/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     # path('admin/', admin.site.urls),
-    path('mywebsite', include('students.urls')),
+    path('mysite', include('students.urls')),
 ]
